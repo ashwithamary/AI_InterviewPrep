60581ac2677b454ee56589a9d816011e29077a7e
Mary Ashwitha Gopu <ashwithamary@gmail.com>
2025-03-26 20:49:05 -0400

    displayed interviews list on dashboard and integrated stripe payment

---
+++ utils/planData.jsx
@@ -0,0 +1,45 @@
+export default [
+    {
+        id: 1,
+        name: 'Free',
+        cost: 0,
+        link: '/',
+        duration: 'Week',
+        offering: [
+            {
+                value: '✔️ Create 3 Free Mock Interviews'
+            },
+            {
+                value: '✔️ Unlimited Interview Retake'
+            },
+            {
+                value: '❌ No Practice Questions'
+            },
+            {
+                value: '❌ No Email Support'
+            },
+        ]
+    },
+    {
+        id: 1,
+        name: 'Monthly',
+        cost: 7.99,
+        link: 'https://buy.stripe.com/test_eVa4ke4D7f62cow6oo',
+        priceId: 'prod_S16L3xyUwQ3Ylb',
+        duration: 'Monthly',
+        offering: [
+            {
+                value: '✔️ Create Unlimited Free Mock Interviews'
+            },
+            {
+                value: '✔️ Unlimited Interview Retake'
+            },
+            {
+                value: '✔️ Unlimited Practice Questions'
+            },
+            {
+                value: '✔️ Unlimited Email Support'
+            },
+        ]
+    }
+]
